886f033da0a3e72afa637919b77c46739911ee93
nicysneiros <nicolle.cysneiros@gmail.com>
2015-01-31 10:05:30 -0300

    Criacão da Camada de Dados

---
+++ app/src/main/java/com/android/quemfaz/QuemFaz.java
@@ -0,0 +1,19 @@
+package com.android.quemfaz;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+/**
+ * Created by nicolle on 31/01/15.
+ */
+public class QuemFaz extends Application {
+
+    public void onCreate(){
+        super.onCreate();
+        Parse.initialize(this, "xC4PRvy2YXmAV2AiQQEYiDYmNQQYvcsVzHQi7fiZ", "XLsk5LfbIrdQl3ViCnf8ka4FcAeNfh1vmkMqEpAW");
+
+        ParseUser.getCurrentUser().saveInBackground();
+    }
+}
---
+++ app/src/main/java/com/android/quemfaz/dados/RepositorioUsuario.java
@@ -0,0 +1,9 @@
+package com.android.quemfaz.dados;
+
+/**
+ * Created by nicolle on 31/01/15.
+ */
+public class RepositorioUsuario {
+
+
+}
