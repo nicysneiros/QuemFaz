188272756cb642829f11fb5a0639120625679878
Rafael <rlcocin@gmail.com>
2015-01-31 18:30:57 -0300

    Criando LoginFragment e CadastrarFragment

---
+++ app/src/main/java/com/android/quemfaz/fragments/CadastrarFragment.java
@@ -0,0 +1,27 @@
+package com.android.quemfaz.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.quemfaz.R;
+
+
+public class CadastrarFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cadastrar, container, false);
+    }
+
+
+}
